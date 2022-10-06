a1dfbc2b237bafe0701747b7cdf5591871a8b748
UzairFahima3 <uzairfahima3@gmail.com>
2022-10-06 15:16:21 +0500

    Multi Tenancy Single Row

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # before_action :set_article, only: [:show, :edit, :update, :destroy]
+  # before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  # private
+  # def article_params
+  #   params.require(:article).permit(:title, :content, :tenant_id)
+  # end
+
+  # def set_author
+  #   @authors = Author.all
+  # end
+end
